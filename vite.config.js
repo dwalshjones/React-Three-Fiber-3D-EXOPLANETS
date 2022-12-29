2c5d05fe3e75e1a5008b3967f2b506a09b02b2ee
Shymala Sidharth <shymala.sidharth22@gmail.com>
2022-12-29 18:49:22 +1300

    index.js removed

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      fastRefresh: process.env.NODE_ENV !== 'test',
-    }),
-  ],
+  plugins: [react()],
   server: {
     watch: {
       usePolling: true,
