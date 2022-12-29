eca5c2d23163117c6a1ad38fd90e1f9a4dbf788f
Shymala Sidharth <shymala.sidharth22@gmail.com>
2022-12-30 09:55:27 +1300

    test10

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:3000',
-    //     changeOrigin: true,
-    //   },
-    // },
-  },
+  // server: {
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  //   host: true,
+  //   strictPort: true,
+  //   // proxy: {
+  //   //   '/api': {
+  //   //     target: 'http://localhost:3000',
+  //   //     changeOrigin: true,
+  //   //   },
+  //   // },
+  // },
 })
