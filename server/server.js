9c1b24d6b1b554ba5b3e11de2ba2f89a661bdc86
Shymala Sidharth <shymala.sidharth22@gmail.com>
2022-12-29 20:30:02 +1300

    test7

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const path = require('path')
-const express = require('express')
+// const path = require('path')
+// const express = require('express')
 
-const server = express()
+// const server = express()
 
-server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
+// server.use(express.json())
+// server.use(express.static(path.join(__dirname, 'public')))
 
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'))
-})
+// server.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'index.html'))
+// })
 
-module.exports = server
+// module.exports = server
